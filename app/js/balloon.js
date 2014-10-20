dc6ce85c6edfebe438efd6f3e834a5fdeecda302
Ningxia Zhang <thmmrs2298@gmail.com>
2014-10-19 21:19:03 -0700

    adding animations

--- app/js/balloon.js
+++ app/js/balloon.js
@@ -18,14 +18,20 @@ var baloon = (function () {
     var style = {
       'left': getRandom(0, 100) + '%',
       'top': getRandom(0, 100) + '%',
-      'transform': 'scale(' + getRandom(1, 40) + ')',
+      'transform': 'scale(0.1) translateX(-300px) translateY(-600px)',
       'color': color,
       'opacity': getRandom(0.1, 0.95)
     };
 
-    $('<div>', {
+    var newCharacter = $('<div>', {
       'class': 'character'
     }).css(style).text(character).appendTo($('body'));
+
+    setTimeout(function () {
+      newCharacter.css('transform', 'scale(' + getRandom(0.1, 1) + ') translateX(-300px) translateY(-600px)');
+    }, 50);
+
+    
   };
 
   var getRandom = function (min, max) {
